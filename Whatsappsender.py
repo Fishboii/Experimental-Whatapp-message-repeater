09d3bfe76f82a602a6f6cd8fd318c3b19091616c
Fishboii <45155521+Fishboii@users.noreply.github.com>
2018-11-28 20:51:36 +0800

    Update Whatsappsender.py

--- Whatsappsender.py
+++ Whatsappsender.py
@@ -4,12 +4,12 @@ import time
 while True:
     try:
         name = input("Name of recipient or group: ")
+        msg = input("Message: ")
         nums = input("Number of messages: ")
-        msg = input(r"Message: ")
         driver = webdriver.Chrome(executable_path=r'C:\Users\junta\Desktop\Selenium drivers\Chromedriver.exe')
         driver.get('https://web.whatsapp.com/')
         input('Hit ENTER after you scanned the QR code.')
-        time.sleep(5)
+        time.sleep(3)
         elem = driver.find_element_by_xpath("//span[@title='{}']".format(name)).click()
         elem = driver.find_element_by_css_selector("._2S1VP.copyable-text.selectable-text")
         for i in range(int(nums)):
